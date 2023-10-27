e3fe77c2c0150a6c7cf9bad8ae41c8bf15c27e77
JaronKing <jaronjking@gmail.com>
2023-10-26 18:02:51 -0700

    Includes images and some polish.

--- src/components/contactStep/ContactDispatch.js
+++ src/components/contactStep/ContactDispatch.js
@@ -6,7 +6,8 @@ import StepButton from "../StepButton";
 const ContactDispatch = ({ children }) => {
 	return (
 		<Step>
-			<div className="flex flex-col px-5 py-32">
+			<div className="flex flex-col px-5 py-10">
+				<img className="w-1/2 rounded-full overflow-hidden m-auto py-10" src="./images/phone.jpeg"/>
                 <div>Phone:</div>
                 <div>(760) 898 - 6275</div>
                 <StepButton stepTemplate="Welcome">
--- src/components/contactStep/WelcomeStep.js
+++ src/components/contactStep/WelcomeStep.js
@@ -6,7 +6,8 @@ import StepButton from "../StepButton";
 const WelcomeStep = ({ children }) => {
 	return (
 		<Step>
-			<div className="flex flex-col px-5 py-32">
+			<div className="flex flex-col px-5 py-10">
+				<img className="w-1/2 rounded-full overflow-hidden m-auto py-10" src="./images/handsShaking.jpeg"/>
                 <StepButton stepTemplate="Trouble">
                 	Need Immediate Assistance
             	</StepButton>
